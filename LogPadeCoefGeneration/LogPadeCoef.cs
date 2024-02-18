dc0deafda4b4bbe4c85e57291b127d535e71f597
tk-yoshimura <yoshimura8192@gmail.com>
2024-02-18 23:29:57 +0900

    Initial commit

---
+++ LogPadeCoefGeneration/LogPadeCoef.cs
@@ -0,0 +1,103 @@
+﻿using System.Numerics;
+
+namespace LogPadeCoefGeneration {
+    public static class LogPadeCoef {
+
+        public static BigInteger[] GenerateCoef(int n) {
+            ArgumentOutOfRangeException.ThrowIfLessThan(n, 2, nameof(n));
+
+            n -= 1;
+
+            Fraction[] c = Solve(n, GenerateMatrix(n), GenerateVector(n));
+            Fraction[] p = [1, .. c[..n], 1, .. c[n..]];
+
+            BigInteger[] v = ToInteger(p);
+
+            return v;
+        }
+
+        static Fraction[,] GenerateMatrix(int n) {
+            Fraction[,] m = new Fraction[2 * n, 2 * n];
+
+            for (int i = 0; i < 2 * n; i++) {
+                for (int j = 0; j < 2 * n; j++) {
+                    m[i, j] = (i == j) ? 1 : 0;
+                }
+            }
+
+            for (int i = 0; i < 2 * n; i++) {
+                Fraction c = new((i % 2 == 0) ? -1 : 1, i + 1);
+
+                for (int j = 0; j < int.Min(n, 2 * n - i); j++) {
+                    m[i + j, j + n] = c;
+                }
+            }
+
+            return m;
+        }
+
+        static Fraction[] GenerateVector(int n) {
+            Fraction[] v = new Fraction[2 * n];
+
+            for (int i = 0; i < 2 * n; i++) {
+                Fraction c = new((i % 2 == 0) ? -1 : 1, i + 2);
+
+                v[i] = c;
+            }
+
+            return v;
+        }
+
+        static Fraction[] Solve(int n, Fraction[,] m, Fraction[] v) {
+            m = (Fraction[,])m.Clone();
+            v = (Fraction[])v.Clone();
+
+            for (int i = n; i < 2 * n; i++) {
+                Fraction mii = m[i, i];
+
+                m[i, i] = 1;
+                for (int j = i + 1; j < 2 * n; j++) {
+                    m[i, j] /= mii;
+                }
+
+                v[i] /= mii;
+
+                for (int j = i + 1; j < 2 * n; j++) {
+                    Fraction mji = m[j, i];
+
+                    m[j, i] = 0;
+
+                    for (int k = i + 1; k < 2 * n; k++) {
+                        m[j, k] -= m[i, k] * mji;
+                    }
+
+                    v[j] -= v[i] * mji;
+                }
+            }
+
+            for (int i = 2 * n - 1; i >= 0; i--) {
+                for (int j = i - 1; j >= 0; j--) {
+                    Fraction mji = m[j, i];
+
+                    for (int k = i; k < 2 * n; k++) {
+                        m[j, k] = 0;
+                    }
+
+                    v[j] -= v[i] * mji;
+                }
+            }
+
+            return v;
+        }
+
+        static BigInteger[] ToInteger(Fraction[] v) {
+            while (v.Any(c => c.Denom > 1)) {
+                BigInteger n = v.First(c => c.Denom > 1).Denom;
+
+                v = v.Select(c => c * n).ToArray();
+            }
+
+            return v.Select(c => c.Numer).ToArray();
+        }
+    }
+}
---
+++ LogPadeCoefGenerationTests/LogPadeCoefTests.cs
@@ -0,0 +1,206 @@
+﻿using LogPadeCoefGeneration;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Numerics;
+
+namespace LogPadeCoefGenerationTests {
+    [TestClass()]
+    public class LogPadeCoefTests {
+        [TestMethod()]
+        public void N4Test() {
+            BigInteger[] actual = LogPadeCoef.GenerateCoef(4);
+
+            foreach (BigInteger c in actual) {
+                Console.WriteLine(c);
+            }
+
+            BigInteger[] expected = [
+                420,
+                510,
+                140,
+                3,
+                420,
+                720,
+                360,
+                48,
+            ];
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void N5Test() {
+            BigInteger[] actual = LogPadeCoef.GenerateCoef(5);
+
+            foreach (BigInteger c in actual) {
+                Console.WriteLine(c);
+            }
+
+            BigInteger[] expected = [
+                3780,
+                6510,
+                3360,
+                505,
+                6,
+                3780,
+                8400,
+                6300,
+                1800,
+                150,
+            ];
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void N8Test() {
+            BigInteger[] actual = LogPadeCoef.GenerateCoef(8);
+
+            foreach (BigInteger c in actual) {
+                Console.WriteLine(c);
+            }
+
+            BigInteger[] expected = [
+                1801800,
+                5825820,
+                7327320,
+                4508350,
+                1395240,
+                198324,
+                9656,
+                35,
+                1801800,
+                6726720,
+                10090080,
+                7761600,
+                3234000,
+                705600,
+                70560,
+                2240,
+            ];
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void N9Test() {
+            BigInteger[] actual = LogPadeCoef.GenerateCoef(9);
+
+            foreach (BigInteger c in actual) {
+                Console.WriteLine(c);
+            }
+
+            BigInteger[] expected = [
+                30630600,
+                114414300,
+                172372200,
+                133963830,
+                56936880,
+                12885180,
+                1382040,
+                51561,
+                140,
+                30630600,
+                129729600,
+                227026800,
+                211891680,
+                113513400,
+                34927200,
+                5821200,
+                453600,
+                11340,
+            ];
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void N16Plot() {
+            const int n = 16;
+
+            BigInteger[] actual = LogPadeCoef.GenerateCoef(n);
+
+            StreamWriter sw = new($"../../../../results/logpadecoef_{n}.txt");
+
+            foreach (BigInteger c in actual) {
+                Console.WriteLine(c);
+                sw.WriteLine(c);
+            }
+
+            sw.WriteLine("eof");
+
+            sw.Close();
+        }
+
+        [TestMethod()]
+        public void N32Plot() {
+            const int n = 32;
+
+            BigInteger[] actual = LogPadeCoef.GenerateCoef(n);
+
+            StreamWriter sw = new($"../../../../results/logpadecoef_{n}.txt");
+
+            foreach (BigInteger c in actual) {
+                Console.WriteLine(c);
+                sw.WriteLine(c);
+            }
+
+            sw.WriteLine("eof");
+
+            sw.Close();
+        }
+
+        [TestMethod()]
+        public void N64Plot() {
+            const int n = 64;
+
+            BigInteger[] actual = LogPadeCoef.GenerateCoef(n);
+
+            StreamWriter sw = new($"../../../../results/logpadecoef_{n}.txt");
+
+            foreach (BigInteger c in actual) {
+                Console.WriteLine(c);
+                sw.WriteLine(c);
+            }
+
+            sw.WriteLine("eof");
+
+            sw.Close();
+        }
+
+        [TestMethod()]
+        public void N128Plot() {
+            const int n = 128;
+
+            BigInteger[] actual = LogPadeCoef.GenerateCoef(n);
+
+            StreamWriter sw = new($"../../../../results/logpadecoef_{n}.txt");
+
+            foreach (BigInteger c in actual) {
+                Console.WriteLine(c);
+                sw.WriteLine(c);
+            }
+
+            sw.WriteLine("eof");
+
+            sw.Close();
+        }
+
+        [TestMethod()]
+        public void N256Plot() {
+            const int n = 256;
+
+            BigInteger[] actual = LogPadeCoef.GenerateCoef(n);
+
+            StreamWriter sw = new($"../../../../results/logpadecoef_{n}.txt");
+
+            foreach (BigInteger c in actual) {
+                Console.WriteLine(c);
+                sw.WriteLine(c);
+            }
+
+            sw.WriteLine("eof");
+
+            sw.Close();
+        }
+    }
+}
